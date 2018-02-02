49d9ad9687e5e11d54a8ef9f424fc7fcfd968999
kaka_08 <602993561@qq.com>
2018-02-02 20:16:14 +0800

    “路由新跳转

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,78 +1,16 @@
 <template>
-<div class="layout">
-        <Layout>
-            <Header>
-                <Menu mode="horizontal" active-name="1">
-                    <div class="layout-logo">
-                      <img src="../assets/images/coca.jpg" class="logo-img" alt="LOGO">
-                    </div>
-                    <div class="layout-nav">
-                        <MenuItem name="1">
-                            <Icon type="ios-navigate"></Icon>
-                            Item 1
-                        </MenuItem>
-                        <MenuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
-                        </MenuItem>
-                        <MenuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
-                        </MenuItem>
-                        <MenuItem name="4">
-                            <Icon type="ios-paper"></Icon>
-                            Item 4
-                        </MenuItem>
-                    </div>
-                </Menu>
-            </Header>
-            <Layout :style="{padding: '0 50px'}">
-                <Content :style="{padding: '24px 0', minHeight: '280px', background: '#fff'}">
-                    <Layout>
-                        <Sider hide-trigger :style="{background: '#fff'}">
-                            <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-                                <Submenu name="1">
-                                    <template slot="title">
-                                        <Icon type="ios-navigate"></Icon>
-                                        Item 1
-                                    </template>
-                                    <MenuItem name="1-1">Option 1</MenuItem>
-                                    <MenuItem name="1-2">Option 2</MenuItem>
-                                    <MenuItem name="1-3">Option 3</MenuItem>
-                                </Submenu>
-                                <Submenu name="2">
-                                    <template slot="title">
-                                        <Icon type="ios-keypad"></Icon>
-                                        Item 2
-                                    </template>
-                                    <MenuItem name="2-1">Option 1</MenuItem>
-                                    <MenuItem name="2-2">Option 2</MenuItem>
-                                </Submenu>
-                                <Submenu name="3">
-                                    <template slot="title">
-                                        <Icon type="ios-analytics"></Icon>
-                                        Item 3
-                                    </template>
-                                    <MenuItem name="3-1">Option 1</MenuItem>
-                                    <MenuItem name="3-2">Option 2</MenuItem>
-                                </Submenu>
-                            </Menu>
-                        </Sider>
-                        <Content :style="{padding: '24px', minHeight: '450px', background: '#fff'}">
-                             <Weathers />
-                             <TestTable />
-                        </Content>
-                    </Layout>
-                </Content>
-            </Layout>
-            <Footer class="layout-footer-center">2016-2018 &copy; Kaka</Footer>
-        </Layout>
-    </div>
+   <div id="app">
+       <transition name="fade"
+                mode="out-in">
+                   <router-view></router-view>
+                  <p @click="toRoutesVue">测试路由，跳转到路由的切换页面</p>        
+                
+    </transition>
+   </div>      
 </template>
 
 <script>
-import Weathers from './Weather.vue';
-import TestTable from './TestTable.vue';
+
 export default {
   data(){
     return{
@@ -81,44 +19,13 @@ export default {
   computed:{
   },
   methods:{
+      toRoutesVue(){
+          this.$router.push({ path: 'testIview' })
+      }
   },
   components: {
-      Weathers,TestTable
   }
 }
 </script>
 
 
-
-
-<style lang="less" scoped>
-  .layout{
-    border: 1px solid #041433;
-    background: #041433;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-}
-.layout-logo{
-    width: 100px;
-    height: 64px;
-    background: #041433;
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 0px;
-    left: 0px;
-}
-.logo-img{
-  width: 100%;
-  height: 100%;
-}
-.layout-nav{
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
-}
-.layout-footer-center{
-    text-align: center;
-}
-</style>
